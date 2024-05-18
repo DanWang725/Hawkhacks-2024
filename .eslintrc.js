851310bb4057d66d6a0718c33486466153544b9a
Daniel Wang <48870892+DanWang725@users.noreply.github.com>
2024-05-18 11:46:41 -0400

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,52 +1,52 @@
 module.exports = {
-  plugins: ["jest"],
+  plugins: ['jest'],
   env: {
     browser: true,
     es2021: true,
-    "jest/globals": true,
+    'jest/globals': true,
   },
-  parser: "@babel/eslint-parser",
+  parser: '@babel/eslint-parser',
 
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
   ],
   overrides: [
     {
       env: {
         node: true,
       },
-      files: [".eslintrc.{js,cjs}"],
+      files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
-        sourceType: "script",
+        sourceType: 'script',
       },
     },
   ],
   parserOptions: {
-    ecmaVersion: "6",
+    ecmaVersion: '6',
     requireConfigFile: false,
     babelOptions: {
-      presets: ["@babel/preset-react"],
+      presets: ['@babel/preset-react'],
     },
   },
   rules: {
-    quotes: [2, "single", { avoidEscape: true, allowTemplateLiterals: true }],
-    "func-style": ["error", "declaration", { allowArrowFunctions: true }],
-    "no-unused-vars": ["warn", { vars: "all" }],
-    "react/react-in-jsx-scope": 0,
-    "react/destructuring-assignment": 2,
-    "react/function-component-definition": [
+    quotes: [2, 'single', { avoidEscape: true, allowTemplateLiterals: true }],
+    'func-style': ['error', 'declaration', { allowArrowFunctions: true }],
+    'no-unused-vars': ['warn', { vars: 'all' }],
+    'react/react-in-jsx-scope': 0,
+    'react/destructuring-assignment': 2,
+    'react/function-component-definition': [
       2,
-      { namedComponents: "arrow-function" },
+      { namedComponents: 'arrow-function' },
     ],
-    "react/prop-types": 0,
-    "no-restricted-imports": [
+    'react/prop-types': 0,
+    'no-restricted-imports': [
       2,
       {
-        name: "axios",
+        name: 'axios',
         message:
-          "Imports of axios package is forbidden. Pwease use axios from /shared-components/axios",
+          'Imports of axios package is forbidden. Pwease use axios from /shared-components/axios',
       },
     ],
   },
